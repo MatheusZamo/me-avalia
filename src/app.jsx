eb5a8ea6e3c39488316ee66e7f8dab919476bde5
MatheusZamo <19matheushenrique03@gmail.com>
2024-11-03 10:13:41 -0300

    Initial rendered design structure

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,49 @@
+const APIKey = "360e928c"
+
+//
+fetch(`http://www.omdbapi.com/?apikey=${APIKey}`)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch(console.log)
+
 const App = () => {
   return (
     <>
       <nav className="nav-bar">
         <img className="logo" src="logo-me-avalia.png" alt="Logo" />
-        <input className="search" type="text" />
-        <p className="num-results">Resultados</p>
+        <form className="form-search">
+          <input
+            className="search"
+            type="text"
+            placeholder="Buscar filmes..."
+          />
+          <button className="btn-search">Buscar</button>
+        </form>
+        <p className="num-results"># Resultados</p>
       </nav>
-      <main className="main"></main>
-      <div className="box"></div>
+      <main className="main">
+        <div className="box">
+          <ul className="list">
+            <button className="btn-toggle">-</button>
+            <li className="list-movies">
+              <img src="https://placehold.co/600x800" alt="" />
+              <h3>Name</h3>
+              <p>🗓️ Year</p>
+            </li>
+          </ul>
+        </div>
+        <div className="box">
+          <div className="summary">
+            <h2>Filmes Assistidos</h2>
+            <div>
+              <p>🎬 0 filmes</p>
+              <p>⏳ 0 min</p>
+            </div>
+            <button className="btn-toggle">-</button>
+          </div>
+          <ul className="list">{/* <li className="list-watched"></li> */}</ul>
+        </div>
+      </main>
     </>
   )
 }
