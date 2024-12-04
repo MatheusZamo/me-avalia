1107afe9977c13a6e4d4da09d6ff8150dcb3ccaa
MatheusZamo <19matheushenrique03@gmail.com>
2024-12-04 20:05:35 -0300

    file created component Main

--- src/app.jsx
+++ src/app.jsx
@@ -1,63 +1,7 @@
 import { useState, useEffect } from "react"
-import { History } from "./components/history"
 import { NavBar } from "./components/nav-bar"
-import { Movies } from "./components/movies"
-import { WatchedMovies } from "./components/watched-movies"
-import { MovieDetails } from "./components/movie-details"
-import { useWatchedMovies } from "./hooks/use-watched-movies"
-import { useClickedMovie } from "./hooks/use-clicked-movie"
+import { Main } from "./components/main"
 import { baseUrl } from "./utils/base-url"
-
-const ListBox = ({ children }) => <div className="box">{children}</div>
-
-const Main = ({ movies }) => {
-  useEffect(() => {
-    setClickedMovie(null)
-  }, [movies])
-
-  const { watchedMovies, setWatchedMovies, handleClickBtnDelete } =
-    useWatchedMovies()
-
-  const {
-    clickedMovie,
-    setClickedMovie,
-    handleClickMovie,
-    handleClickBtnBack,
-    handleSubmitRating,
-  } = useClickedMovie(setWatchedMovies)
-
-  return (
-    <main className="main">
-      <ListBox>
-        <Movies movies={movies} onClickMovie={handleClickMovie} />
-      </ListBox>
-      <ListBox>
-        {clickedMovie ? (
-          <MovieDetails
-            clickedMovie={clickedMovie}
-            onSubmitRating={handleSubmitRating}
-            onClickBtnBack={handleClickBtnBack}
-          />
-        ) : (
-          <>
-            <History
-              watchedMovies={watchedMovies}
-              onClickBtnBack={handleClickBtnBack}
-              onSubmitRating={handleSubmitRating}
-            />
-
-            {watchedMovies.length > 0 && (
-              <WatchedMovies
-                watchedMovies={watchedMovies}
-                onClickBtnDelete={handleClickBtnDelete}
-              />
-            )}
-          </>
-        )}
-      </ListBox>
-    </main>
-  )
-}
 
 const App = () => {
   const [movies, setMovies] = useState([])
