4b64325d2203599c3101f60f0be9a4682a504bc9
MatheusZamo <19matheushenrique03@gmail.com>
2024-12-10 16:38:21 -0300

    displaying paragraph when starting the app, searching for a movie, and when clicking on the movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,20 +1,39 @@
+import { useState, useEffect } from "react"
 import { getMoviePoster } from "@/utils/get-movie-poster"
 
-const Movies = ({ onClickMovie, movies }) => (
-  <ul className="list list-movies">
-    <button className="btn-toggle">-</button>
+const Movies = ({ onClickMovie, movies }) => {
+  const [showMovies, setShowMovies] = useState(true)
 
-    {movies?.map((movie) => (
-      <li key={movie.id} onClick={() => onClickMovie(movie)}>
-        <img src={getMoviePoster(movie.poster)} alt="" />
-        <h3>{movie.title}</h3>
-        <p>
-          <span>🗓️</span>
-          <span>{movie.year}</span>{" "}
-        </p>
-      </li>
-    ))}
-  </ul>
-)
+  useEffect(() => {
+    setTimeout(() => {
+      setShowMovies(false)
+    }, 1000)
+
+    return () => setShowMovies(true)
+  }, [movies])
+
+  return (
+    <>
+      {showMovies ? (
+        <p className="loader">Carregando...</p>
+      ) : (
+        <ul className="list list-movies">
+          <button className="btn-toggle">-</button>
+
+          {movies?.map((movie) => (
+            <li key={movie.id} onClick={() => onClickMovie(movie)}>
+              <img src={getMoviePoster(movie.poster)} alt="" />
+              <h3>{movie.title}</h3>
+              <p>
+                <span>🗓️</span>
+                <span>{movie.year}</span>{" "}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  )
+}
 
 export { Movies }
